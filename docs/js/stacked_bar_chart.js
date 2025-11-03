b60e6a9ea9558de11fd7cfa91d0e61aaa4480577
Isabella Tagliafico <78494570+Isabibbi@users.noreply.github.com>
2025-11-03 15:38:04 +0100

    Update stacked_bar_chart.js

--- docs/js/stacked_bar_chart.js
+++ docs/js/stacked_bar_chart.js
@@ -1,5 +1,142 @@
 function drawStackedBarChart(rawData) {
-    (async function () {
-        console.log("Stacked Bar Chart Data:", rawData);
-    })();
+  (async function () {
+
+    const container       = d3.select("#stacked_bar_chart_container");
+    const svgContainer    = container.select("#stacked_bar_chart_svg");
+    const legendContainer = container.select("#stacked_bar_chart_legend");
+    const tooltip         = container.select("#stacked_bar_chart_tooltip");
+
+    svgContainer.selectAll("*").remove();
+    legendContainer.selectAll("*").remove();
+
+    const margin = { top:40, right:140, bottom:40, left:180 };
+    const width  = 800 - margin.left - margin.right;
+    const height = 500 - margin.top - margin.bottom;
+
+    const svg = svgContainer.append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform", `translate(${margin.left},${margin.top})`);
+
+    // Keys and colors
+    let allKeys = Object.keys(rawData[0]).filter(d => d !== "region_txt");
+    const palette_map = { "Other":"#8c564b", "Unknown":"#2c2c2c" };
+    const default_palette = ["#1f77b4","#ff7f0e","#2ca02c","#d62728","#9467bd"];
+    const color = d => palette_map[d] || default_palette[ allKeys.indexOf(d) % default_palette.length ];
+
+    const y = d3.scaleBand()
+      .domain(rawData.map(d => d.region_txt))
+      .range([0,height])
+      .padding(0.3);
+
+    const x = d3.scaleLinear()
+      .domain([0,100])
+      .range([0,width]);
+
+    // Axes
+    svg.append("g").call(d3.axisLeft(y));
+    svg.append("g")
+      .attr("transform",`translate(0,${height})`)
+      .call(d3.axisBottom(x).ticks(5).tickFormat(d=>d+"%"));
+
+    const LITERAL_GREY = "#bfbfbf";
+
+    // Function to compute stacked positions
+    function computeStackedPositions(data, keysOrder) {
+      return data.map(row => {
+        let cum = 0;
+        const stackedRow = { region_txt: row.region_txt };
+        keysOrder.forEach(k => {
+          stackedRow[k] = { x0: cum, x1: cum + row[k] };
+          cum += row[k];
+        });
+        return stackedRow;
+      });
+    }
+
+    let currentData = computeStackedPositions(rawData, allKeys);
+
+    // Draw bars
+    function drawBars(stackedRows) {
+      svg.selectAll(".layer").remove();
+
+      allKeys.forEach(key => {
+        const group = svg.append("g")
+          .attr("class","layer")
+          .attr("data-key", key)
+          .attr("fill", color(key));
+
+        group.selectAll("rect")
+          .data(stackedRows.map(d => ({ region_txt: d.region_txt, ...d[key] })))
+          .enter()
+          .append("rect")
+          .attr("y", d => y(d.region_txt))
+          .attr("x", d => x(d.x0))
+          .attr("width", d => x(d.x1 - d.x0))
+          .attr("height", y.bandwidth())
+          .on("mouseover", function(event,d){
+            tooltip
+              .style("opacity",1)
+              .html(`${d.region_txt}<br>${key}: ${(d.x1-d.x0).toFixed(1)}%`)
+              .style("left",(event.pageX+10)+"px")
+              .style("top",(event.pageY-20)+"px");
+          })
+          .on("mouseout", () => tooltip.style("opacity",0));
+      });
+    }
+
+    drawBars(currentData);
+
+    // Legend
+    // Legend
+legendContainer.selectAll("span")
+  .data(allKeys)
+  .enter()
+  .append("span")
+  .style("display","inline-block")
+  .style("margin-right","12px")
+  .style("cursor","pointer")
+  .html(d=>`<span style="background:${color(d)};width:14px;height:14px;display:inline-block;margin-right:4px;"></span>${d}`)
+  .on("click", function(event, key){
+    const isActive = d3.select(this).classed("active");
+    legendContainer.selectAll("span").classed("active", false);
+
+    if(isActive){
+      // deselect: restore original
+      currentData = computeStackedPositions(rawData, allKeys);
+
+      // restore colors
+      allKeys.forEach(k => {
+        svg.select(`g[data-key='${k}']`).selectAll("rect")
+          .transition().duration(500)
+          .attr("x", d => x(d.x0))
+          .attr("width", d => x(d.x1 - d.x0))
+          .style("fill", color(k));
+      });
+
+    } else {
+      d3.select(this).classed("active", true);
+
+      // roll selected key to the left
+      currentData = rawData.map(row => {
+        const idx = allKeys.indexOf(key);
+        const order = allKeys.slice(idx).concat(allKeys.slice(0, idx));
+        return computeStackedPositions([row], order)[0];
+      });
+
+      // Animate bars and set colors: selected keeps color, others gray
+      allKeys.forEach(k => {
+        svg.select(`g[data-key='${k}']`).selectAll("rect")
+          .data(currentData.map(d => ({ region_txt: d.region_txt, ...d[k] })))
+          .transition().duration(500)
+          .attr("x", d => x(d.x0))
+          .attr("width", d => x(d.x1 - d.x0))
+          .style("fill", k === key ? color(k) : "#bfbfbf"); // gray out others
+      });
+    }
+  });
+
+
+  })();
 }
